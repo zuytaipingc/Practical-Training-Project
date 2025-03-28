ba0482793ca0034fd54bba64aa06162f670b9fd3
zuytaipingc <627097073@qq.com>
2025-03-28 08:31:42 +0800

    Add files via upload

---
+++ ch04_3/src/main/java/com/css0115/dao/IdCardMapper.java
@@ -0,0 +1,8 @@
+package com.css0115.dao;
+
+import com.css0115.pojo.IdCard;
+import org.apache.ibatis.annotations.Param;
+
+public interface IdCardMapper {
+    IdCard findCodeById(@Param("id")Integer id);
+}
