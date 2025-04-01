a195fcdca9cd357ffb8c7e1ff07d6a8175e39955
zuytaipingc <627097073@qq.com>
2025-04-01 13:57:55 +0800

    Delete ch04_3 directory

--- ch04_3/src/main/java/com/css0115/dao/IdCardMapper.java
+++
@@ -1,8 +0,0 @@
-package com.css0115.dao;
-
-import com.css0115.pojo.IdCard;
-import org.apache.ibatis.annotations.Param;
-
-public interface IdCardMapper {
-    IdCard findCodeById(@Param("id")Integer id);
-}
